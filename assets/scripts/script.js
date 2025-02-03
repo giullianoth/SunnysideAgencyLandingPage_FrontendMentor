12f8eb5cb66d9fcf93727c08fc61447cd88881a9
Giulliano <tharsoweb@gmail.com>
2025-02-03 17:43:16 -0300

    refactoring code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,5 @@
 // MOBILE MENU
-const mobileMenu = document.querySelector(".j_mobile_menu");
-const mobileMenuIcon = document.querySelector(".j_mobile_menu_icon");
+const mobileMenu = document.querySelector(".j_mobile_menu")
+const mobileMenuIcon = document.querySelector(".j_mobile_menu_icon")
 
-mobileMenuIcon.addEventListener("click", () => {
-    if (!mobileMenu.classList.contains("active")) {
-        mobileMenu.classList.add("active");
-        setTimeout(() => {
-            mobileMenu.style.transform = "translateY(0)";
-            mobileMenu.style.opacity = 1;
-        }, 50);
-    } else {
-        mobileMenu.style.transform = "";
-        mobileMenu.style.opacity = "";
-        setTimeout(() => {
-            mobileMenu.classList.remove("active");
-        }, 300);
-    }
-})
+mobileMenuIcon.addEventListener("click", () => mobileMenu.classList.toggle("open"))
